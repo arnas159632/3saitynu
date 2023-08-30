c3bd0ff9d4bd3f9c3636e4d692e3baab0e10debd
arnas159632 <89695189+arnas159632@users.noreply.github.com>
2023-08-30 12:50:54 +0300

    Delete _3PraktinisDarbasSaitynai directory

--- _3PraktinisDarbasSaitynai/src/main/java/org/lt/viko/eif/adrevinskas/books/exception/ServerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.lt.viko.eif.adrevinskas.books.exception;
-
-public class ServerNotFoundException extends RuntimeException{
-    public ServerNotFoundException(String message){
-        super ("Error: " + message);
-    }
-}
--- _3PraktinisDarbasSaitynai/src/main/java/org/lt/viko/eif/adrevinskas/books/repository/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package org.lt.viko.eif.adrevinskas.books.repository;
-
-import org.lt.viko.eif.adrevinskas.books.models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-}
